4fccd59426f8663fb58b1e27172420e151bbcfbd
ninobevin <ninobevin@gmail.com>
2023-10-24 11:45:59 +0800

    updated

--- src/resources/js/components/App.vue
+++ src/resources/js/components/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-app>
-<root-nav></root-nav>   
- <v-content> 
- <router-view></router-view>
- </v-content>
-  </v-app>
+    <v-app>
+        <root-nav></root-nav>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app>
 </template>
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    mounted() {
+        console.log("Component mounted.");
     }
+};
 </script>
